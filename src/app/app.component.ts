b22e1a7aa5b57aa32879e13f40fac94ecc11d3ea
Конов Дмитрий <konovdy@it2g.ru>
2024-01-31 21:58:05 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,155 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexDataLabels,
+  ApexTooltip,
+  ApexStroke,
+} from 'ng-apexcharts';
+import * as echarts from 'echarts';
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  stroke: ApexStroke;
+  tooltip: ApexTooltip;
+  dataLabels: ApexDataLabels;
+};
+type EChartsOption = echarts.EChartsOption;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'charts';
+export class AppComponent implements OnInit {
+  @ViewChild('chart') chart: ChartComponent;
+
+  public chartOptions!: Partial<ChartOptions> | any;
+  public option: EChartsOption;
+  public emissionsGroup: FormGroup;
+
+  private coal: number[] = [];
+  private gas: number[] = [];
+  private summary: number[] = [];
+  private dates: string[] = [];
+
+  public ngOnInit(): void {
+    this.initFormGroup();
+    this.generateSpline();
+    this.generateScratter();
+  }
+
+  public add(): void {
+    const gas =
+      this.emissionsGroup.controls['cubicMeters'].value * 1.129 * 1.59;
+    const coal = this.emissionsGroup.controls['tons'].value * 0.768 * 2.76;
+    this.coal.push(coal);
+    this.gas.push(gas);
+    this.summary.push(gas + coal);
+
+    const datePipe = new DatePipe('en-US');
+    const date = datePipe.transform(
+      new Date(this.emissionsGroup.controls['date'].value),
+      'MM/dd/yyyy'
+    );
+
+    this.dates.push(date as string);
+    this.emissionsGroup.reset();
+    this.generateSpline();
+    this.generateScratter();
+  }
+
+  private generateSpline(): void {
+    this.chartOptions = {
+      series: [
+        {
+          name: 'газ',
+          data: this.gas,
+        },
+        {
+          name: 'Сумма',
+          data: this.summary,
+        },
+      ],
+      chart: {
+        height: 350,
+        type: 'area',
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        curve: 'smooth',
+      },
+      xaxis: {
+        type: 'datetime',
+        categories: this.dates,
+      },
+      tooltip: {
+        x: {
+          format: 'MM/dd/yyyy',
+        },
+      },
+      title: {
+        text: 'Выбросы газа',
+        align: 'left',
+      },
+    };
+  }
+
+  private generateScratter(): void {
+    const chartDom = document.getElementById('main');
+    const myChart = echarts.init(chartDom);
+    var option: EChartsOption;
+
+    option = {
+      title: {
+        text: 'Выбросы угля',
+      },
+      legend: {
+        bottom: 5,
+      },
+      xAxis: {
+        type: 'category',
+        data: this.dates,
+        name: 'время',
+      },
+      yAxis: {
+        name: 'тонны',
+      },
+      series: [
+        {
+          name: 'Уголь',
+          symbolSize: 20,
+          data: this.coal,
+          type: 'scatter',
+        },
+        {
+          name: 'Сумма',
+          symbolSize: 20,
+          data: this.summary,
+          type: 'scatter',
+        },
+      ],
+    };
+
+    option && myChart.setOption(option);
+  }
+
+  private initFormGroup(): void {
+    this.emissionsGroup = new FormGroup({
+      tons: new FormControl(null, [Validators.required, Validators.max(1000)]),
+      cubicMeters: new FormControl(null, [
+        Validators.required,
+        Validators.max(1000),
+      ]),
+      date: new FormControl('', [Validators.required]),
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { NgApexchartsModule } from 'ng-apexcharts';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule,
+    NgApexchartsModule,
+    MatButtonModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
